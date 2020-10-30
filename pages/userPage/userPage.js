d38b2caec11b64bb549e1e7476ecd1edf21f6462
fxh-hhh <72294133+fxh-hhh@users.noreply.github.com>
2020-10-30 22:22:55 +0800

    Add files via upload

---
+++ pages/userPage/userPage.js
@@ -0,0 +1,85 @@
+// pages/userPage/userPage.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    listnames:[
+      {
+        title:"我的收藏",
+        listUrl:"./../userPage/userCollections/userCollections",
+      },
+      {
+        title:"我的创作",
+        listUrl:"./../userPage/userCreations/userCreations",
+      },
+      {
+        title:"近期浏览",
+        listUrl:"./../userPage/recentBrowse/recentBrouse",
+      },
+      {
+        title:"联系我们",
+        listUrl:"./../userPage/contactDeveloper/contactDeveloper",
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+
+
+});
