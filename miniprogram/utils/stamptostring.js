b9ebcb3ca5e799932ce608224464dd6c6cd89869
仙 神 <2776835685@qq.com>
2020-11-01 00:03:42 +0800

    修复已知bug

--- miniprogram/utils/stamptostring.js
+++ miniprogram/utils/stamptostring.js
@@ -1,8 +1,8 @@
-export function stamptostring(stamp){
+export function stamptostring(stamp) {
     var date = new Date(stamp); //时间戳为10位需*1000，时间戳为13位的话不需乘1000
     const fill = v => (v < 10 ? '0' : '') + v;
     var Y = date.getFullYear();
-    var M = fill(date.getMonth());
+    var M = fill(date.getMonth() + 1);
     var D = fill(date.getDate());
     var h = fill(date.getHours());
     var m = fill(date.getMinutes());
